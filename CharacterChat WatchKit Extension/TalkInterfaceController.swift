2e44f259f1eab45ccb53cf0a80a602005a3ddae9
Takuya Yokoyama <takyokoy@yahoo-corp.jp>
2016-07-16 15:48:00 +0900

    3D表示できるとこ

---
+++ CharacterChat WatchKit Extension/TalkInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  TalkInterfaceController.swift
+//  CharacterChat
+//
+//  Created by Takuya Yokoyama on 2016/07/16.
+//  Copyright © 2016年 Yahoo Japan Corporation. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class TalkInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: AnyObject?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
